8329445a3bd3dca106098019cf92b9d8d6b8acb4
Ashaduzzaman Rifat <rftzmn1@gmail.com>
2025-06-15 07:20:02 +0600

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='footer'>
-      <small>&copy; Copyright by ByteGrad. All rights reserved</small>
+      <small>&copy; Copyright by Rifat. All rights reserved</small>
       <small>Last checked limits: 17 days ago</small>
     </footer>
   );
